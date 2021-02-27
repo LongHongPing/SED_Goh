fc1fcb6c680d772086816d855bedbaec01ca2dbe
hplong <307527577@qq.com>
2021-02-27 17:14:07 +0800

    edit BloomFilter and EncUtil

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,36 +1,56 @@
+import java.util.ArrayList;
 import java.util.BitSet;
 
 /** 布隆过滤器 */
 public class BloomFilter {
     private static final int DEFAULT_SIZE = 2 << 24;
     private static final int[] seeds = new int[] { 3,5, 7, 11, 13, 31, 37, 61 };  //质数可以更好的防止hash冲突,位数越多误判率越低
-    private BitSet bits = new BitSet(DEFAULT_SIZE);
+    private boolean[] bits = new boolean[DEFAULT_SIZE];
     private SimpleHash[] func = new SimpleHash[seeds.length];
 
+    private static int cnt = 1;
+    /** 布隆过滤器 */
     public BloomFilter() {
         for (int i = 0; i < seeds.length; i++) {
             func[i] = new SimpleHash(DEFAULT_SIZE, seeds[i]);
         }
     }
-    /** 添加内容 */
-    public void add(String value) {
-        for (SimpleHash f : func) {
-            bits.set(f.hash(value), true);
+    /** 映射码字到过滤器相应位置 */
+    public static int[] findPosition(byte[][] codeWords,int filterSize){
+        int[] indexPositions = new int[cnt];
+        for(byte[] codeWord : codeWords){
+            int x = 0;
+            indexPositions = append(indexPositions,x%DEFAULT_SIZE);
         }
+        return indexPositions;
+    }
+    /** 追加内容 */
+    private static int[] append(int[] values, int value) {
+
+        return values;
+    }
+    /** 添加内容 */
+    public void add(byte[][] codeWords) {
+       int[] position = findPosition(codeWords,DEFAULT_SIZE);
+        for (int i : position) {
+            bits[i] = true;
+        }
+
     }
     /** 检验内容 */
-    public boolean check(String value) {
+    public boolean check(byte[][] value) {
         if (value == null) {
             return false;
         }
         boolean ret = true;
-        for (SimpleHash f : func) {
-            ret = ret && bits.get(f.hash(value));
+        int[] position = findPosition(value,DEFAULT_SIZE);
+        for (int i : position) {
+            ret = ret && bits[i];
         }
         return ret;
     }
 
-    /** 内部类，simpleHash */
+    /** 内部类，Hash函数 */
     public static class SimpleHash {
         private int cap;
         private int seed;
--- src/EncUtil.java
+++ src/EncUtil.java
@@ -1,2 +1,105 @@
+/** 加密工具类 */
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class EncUtil {
+    public static EncUtil encUtil;
+    public static byte[][] trapDoors;
+    public static byte[][] codeWords;
+    public static BloomFilter index;
+
+    private static int cnt = 1;
+    /** 懒汉式单例 双重锁*/
+    private EncUtil(){
+
+    }
+    public static EncUtil getInstance(){
+        if(encUtil == null){
+            synchronized (EncUtil.class){
+                if(encUtil == null){
+                    encUtil = new EncUtil();
+                }
+            }
+        }
+        return encUtil;
+    }
+    /** 生成流加密 */
+    public static byte[] genStreamCipher(String key,String NONCE,String counter,String target){
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE,new SecretKeySpec(key.getBytes(),"AES"),new IvParameterSpec(HexUtil.hexToByte(NONCE+counter)));
+            return cipher.doFinal(target.getBytes("UTF-8"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /** 加密string类型词语 */
+    public static byte[] aesEncrypt(String key,String target,String iv){
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE,new SecretKeySpec(key.getBytes(),"AES"),new IvParameterSpec(iv.getBytes("UTF-8")));
+            return cipher.doFinal(target.getBytes("UTF-8"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /** 加密byte类型词语 */
+    public static byte[] aesEncrypt(String key,byte[] target,String iv){
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE,new SecretKeySpec(key.getBytes(),"AES"),new IvParameterSpec(iv.getBytes("UTF-8")));
+            return cipher.doFinal(target);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /** 生成随机字节数组 */
+    public static byte[] genRandomBytes(int n){
+        byte[] byteArray = new byte[n];
+        new Random(0).nextBytes(byteArray);
+        return byteArray;
+    }
+    /** 追加内容 */
+    public static byte[][] append(byte[][] byts, byte[] byt) {
+
+        return byts;
+    }
+    /** 创建HMAC */
+    public static byte[] createHmac(String data,byte[] key) throws Exception{
+        Mac Hmac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key, "HmacSHA256");
+        Hmac.init(secret_key);
+        return Hmac.doFinal(data.getBytes("UTF-8"));
+    }
+    /** 创建陷门、码字 */
+    public static byte[][] buildTrapCode(String keyword,byte[][] keys)throws Exception{
+        byte[][] trapCodes = new byte[cnt][];
+        for(byte[] key : keys){
+            byte[] trapCode = createHmac(keyword,key);
+            trapCodes = append(trapCodes,trapCode);
+        }
+        return trapCodes;
+    }
+
+    /** 建立 */
+    public static void build(String fileName,String keyword,byte[][] keys)throws Exception{
+        trapDoors = buildTrapCode(keyword,keys);
+        codeWords = buildTrapCode(fileName,trapDoors);
+    }
+    /** 安全索引盲化 */
+    public static void blind(int numKeywords,int docSize,int numKeys){
+        int blindFactor = (docSize - numKeywords) * numKeys;
+        //ArrayList<ArrayList<Byte>> blinding = new ArrayList<>();
+        byte[][] blinding = new byte[cnt][];
+        byte[] randomBytes = genRandomBytes(blindFactor);
+        blinding = append(blinding,randomBytes);
+        index.add(blinding);
+    }
 }
